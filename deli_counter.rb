c9f299786213bbb14e49e0a59c0a25e10727a2e9
michael hohl <mhohl@rocketmail.com>
2018-03-19 20:25:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,10 @@ katz_deli = []
 def line(array)
   if array.length == 0 or array.nil?
     puts "The line is currently empty."
-
-  def take_a_number(array, string)
-    array << string
-    puts "Welcome, #{string}. You are number #{array.size} in line."
   end
+end
+
+def take_a_number(array, string)
+  array << string
+  puts "Welcome, #{string}. You are number #{array.size} in line."
+end
